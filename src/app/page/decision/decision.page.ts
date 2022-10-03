5f10bf37766baa4d43bce0881114309d874e672c
Pietro Grazzioli Golfeto <pggolfeto@gmail.com>
2022-10-02 21:54:06 -0300

    textos da decisao

--- src/app/page/decision/decision.page.ts
+++ src/app/page/decision/decision.page.ts
@@ -24,7 +24,7 @@ export class DecisionPage implements OnInit {
 
   public getText(){
     if(this.iterator===0){
-      this.textts = 'Welcome engineer, I am chief engineer. Seems like you are one of the best in your field and also NASAs new hire.';
+      this.textts = 'Welcome, engineer. I am chief engineer Parker. Seems like you are one of the best in your field and also NASAs new hire.';
     }
     else if (this.iterator===1){
       this.textts = 'Congrats! Brace yourself for a lifetime of challenges!';
@@ -33,7 +33,7 @@ export class DecisionPage implements OnInit {
       this.textts = 'You will help us design our future Solar Probe Parker, that will contribute to reveal the mysteries of our Sun.';
     }
     else if (this.iterator===3){
-      this.textts = 'Before we start, what is your name engineer?';
+      this.textts = 'Before we start, what is your name?';
       this.condition=2;
     }
     else if (this.iterator===4){
@@ -55,11 +55,11 @@ export class DecisionPage implements OnInit {
       this.condition=1;
     }
     else if (this.iterator===7 && this.c===1){
-      this.textts='Interesting idea '+ this.name + ' .There is a few considerations I would like you to make:';
+      this.textts='Interesting idea '+ this.name + ' .There are a few considerations I would like you to make:';
       this.condition = 0;
     }
     else if (this.iterator===8 && this.c===1){
-      this.textts='we need a material that can resist the Suns heat and protect our equipment in the probe.';
+      this.textts='We need a material that can resist the Suns heat and protect our equipment in the probe.';
     }
     else if (this.iterator===7 && this.c===0){
       this.textts='Good pick '+ this.name +' .Carbon composites are great at resisting heat and are lighter than steel,';
@@ -68,7 +68,7 @@ export class DecisionPage implements OnInit {
       this.textts='We also require a light material to be able to send it to space.';
     }
     else if (this.iterator===8 && this.c===0){
-      this.textts='so they will help us lift the probe using less energy. Let us go with it then.';
+      this.textts='So they will help us lift the probe using less energy. Let us go with it then.';
     }
     else if (this.iterator===10 && this.c===1){
       this.textts='Stainless steel is not good at dispersing heat efficiently and it is very heavy to lift to space,';
